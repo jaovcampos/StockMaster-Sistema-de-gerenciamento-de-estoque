59d3f7b72afb04085bc198cc01b6b8aef3be9c5e
jaovcampos <cscjoao@hotmail.com>
2024-06-26 18:07:02 -0300

    att.

--- StockMaster-Sistema-de-gerenciamento-de-estoque/src/main.cpp
+++ StockMaster-Sistema-de-gerenciamento-de-estoque/src/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include "monitoramento_estoque/include/monitor.h"
 
 int main() {
-    std::cout << "StockMaster Sistema de Gerenciamento de Estoque" << std::endl;
+    MonitoramentoEstoque estoque;
+
+    estoque.adicionarItem("ProdutoA", 50);
+    estoque.adicionarItem("ProdutoB", 20);
+    estoque.removerItem("ProdutoA", 10);
+    estoque.imprimirEstoque();
+
+    int quantidadeProdutoB = estoque.verificarItem("ProdutoB");
+    std::cout << "Quantidade de ProdutoB no estoque: " << quantidadeProdutoB << "\n";
+
     return 0;
 }
