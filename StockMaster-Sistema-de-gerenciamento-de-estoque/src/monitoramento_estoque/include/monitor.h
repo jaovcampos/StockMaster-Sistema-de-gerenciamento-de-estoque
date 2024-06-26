6ff3bb120b5c1462469ccdd682055fbadad37fe5
jaovcampos <cscjoao@hotmail.com>
2024-06-26 17:24:15 -0300

    att.

--- StockMaster-Sistema-de-gerenciamento-de-estoque/src/monitoramento_estoque/include/monitor.h
+++ StockMaster-Sistema-de-gerenciamento-de-estoque/src/monitoramento_estoque/include/monitor.h
@@ -1,6 +1,23 @@
+// monitor.h
 #ifndef MONITOR_H
 #define MONITOR_H
 
-// Funções de monitoramento de estoque
+#include <string>
+#include <vector>
+#include <map>
+
+class Monitor {
+public:
+    Monitor();
+    ~Monitor();
+
+    void addProduct(std::string productName, int quantity);
+    void removeProduct(std::string productName, int quantity);
+    int getQuantity(std::string productName);
+    std::vector<std::string> getProducts();
+
+private:
+    std::map<std::string, int> products_;
+};
 
 #endif // MONITOR_H
